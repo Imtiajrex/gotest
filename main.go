48de7a42962b1e90c5ef42aa5cf15f0708cdc01c
imtiaj <imtiajrex@gmail.com>
2024-06-05 14:23:52 +0600

    updated port

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ func main() {
     http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprintf(w, "Hello, World!")
     })
-    fmt.Printf("Server running (port=3000), route: http://localhost:3000/helloworld\n")
-    if err := http.ListenAndServe(":3000", nil); err != nil {
+    fmt.Printf("Server running (port=5000), route: http://localhost:5000/helloworld\n")
+    if err := http.ListenAndServe(":5000", nil); err != nil {
         log.Fatal(err)
     }
 }
